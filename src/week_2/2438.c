b1f9714994c99156d19a2492da8ecb7e557234f4
ChoInHeum <heumi711@gmail.com>
2026-04-15 18:18:38 +0900

    2438

---
+++ src/week_2/2438.c
@@ -0,0 +1,18 @@
+// 1번 문제 (별 찍기 - 1)
+
+#include <stdio.h>
+
+int main() {
+    int input;
+
+    scanf("%d", &input);
+
+    for (int i = 0; i < input; i++) {
+        for (int j = 0; j < i + 1; j ++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
